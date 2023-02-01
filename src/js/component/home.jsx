f14af5d08d12df24eb36c1946dbc25b00a84f188
moncherry369 <118934984+moncherry369@users.noreply.github.com>
2023-02-01 18:22:16 -0500

    Completed Seconds counter with countdown.. no btns

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { SecondsCounter } from "./Counter.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//Component -- Timer 
 const Home = () => {
+	const [counter, setCounter] = useState(0);
+    const [counterStr, setCounterStr] = useState(counter.toString().padStart(6, "0"));
+    const [countDown, setCountDown] = useState(0);
+    const [alertTarget, setAlertTarget] = useState(0);
+    const [timer, setTimer] = useState(1000);
+const handleTimer = () => {
+    setTimer = 100000;
+ };
+ const resumeTimer = () => {
+    setTimer = 1000;
+ };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (!countDown) {
+                setCounter((prevCounter) => prevCounter + 1);
+                setCounterStr(counter.toString().padStart(6, "0"));
+            }
+            else if (countDown) {
+                setCountDown(countDown - 1);
+                setCounter((countDown) => countDown - 1);
+                setCounterStr(countDown.toString().padStart(6, "0"));
+            }
+            else if (alertTarget) { //I don't think this is working//
+                setAlertTarget(alertTarget + 1);
+                setCounter((alertTarget) => alertTarget + 1);
+                
+                if (counter === alertTarget) {
+                    alert("Number Reached!");
+                }
+            }
+        },1000);
+
+    }, [counter]);
+
+//Buttons do not work. Try figuring them out!!//
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<SecondsCounter counterStr = {counterStr} />
+            <h5>Countdown</h5>
+			<input className="input" placeholder="Enter a Number" type="number" onChange={e => setCountDown(e.target.value)} />
+          
+            <div className="btn-container">
+                <button className="buttons"  onlclick={handleTimer}>Stop</button>
+                <button className="buttons">Reset</button>
+                <button className="buttons"  onclick={resumeTimer}>Resume</button>
+            </div>
 		</div>
 	);
 };
+
+
+
 
 export default Home;
